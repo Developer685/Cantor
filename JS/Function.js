0b76d850df7450e8401df6cccbc1821324386bed
Jakub Kulik <jakub.kulik1@o2.pl>
2023-09-07 10:21:21 +0200

    added js functions

--- JS/Function.js
+++ JS/Function.js
@@ -5,7 +5,9 @@ let optionResultElement = document.querySelector(".js-option-result");
 let reslut = document.querySelector(".js-result");
 
 
-
+const welcome = () => {
+    console.log("Hello Developer");
+}
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -14,56 +16,58 @@ formElement.addEventListener("submit", (event) => {
     let optionResult = optionResultElement.value;
     let exchangeResult = 0;
 
+    const change = () => {
+        switch (true) {
+            case (optionInput === "EUR" && optionResult === "EUR"):
+                exchangeResult = input * 1;
+                reslut.innerText = exchangeResult;
+                break;
 
-    switch (true) {
-        case (optionInput === "EUR" && optionResult === "EUR"):
-            exchangeResult = input * 1;
-            reslut.innerText = exchangeResult;
-            break;
+            case (optionInput === "EUR" && optionResult === "USD"):
+                exchangeResult = input * 1.1;
+                reslut.innerText = exchangeResult;
+                break;
 
-        case (optionInput === "EUR" && optionResult === "USD"):
-            exchangeResult = input * 1.1;
-            reslut.innerText = exchangeResult;
-            break;
+            case (optionInput === "EUR" && optionResult === "PLN"):
+                exchangeResult = input * 4.43;
+                reslut.innerText = exchangeResult;
+                break;
 
-        case (optionInput === "EUR" && optionResult === "PLN"):
-            exchangeResult = input * 4.43;
-            reslut.innerText = exchangeResult;
-            break;
+            case (optionInput === "USD" && optionResult === "USD"):
+                exchangeResult = input * 1;
+                reslut.innerText = exchangeResult;
+                break;
 
-        case (optionInput === "USD" && optionResult === "USD"):
-            exchangeResult = input * 1;
-            reslut.innerText = exchangeResult;
-            break;
+            case (optionInput === "USD" && optionResult === "EUR"):
+                exchangeResult = input * 0.91;
+                reslut.innerText = exchangeResult;
 
-        case (optionInput === "USD" && optionResult === "EUR"):
-            exchangeResult = input * 0.91;
-            reslut.innerText = exchangeResult;
+                break;
 
-            break;
+            case (optionInput === "USD" && optionResult === "PLN"):
+                exchangeResult = input * 4.04;
+                reslut.innerText = exchangeResult;
+                break;
 
-        case (optionInput === "USD" && optionResult === "PLN"):
-            exchangeResult = input * 4.04;
-            reslut.innerText = exchangeResult;
-            break;
+            case (optionInput === "PLN" && optionResult === "PLN"):
+                exchangeResult = input * 1;
+                reslut.innerText = exchangeResult;
 
-        case (optionInput === "PLN" && optionResult === "PLN"):
-            exchangeResult = input * 1;
-            reslut.innerText = exchangeResult;
+                break;
 
-            break;
+            case (optionInput === "PLN" && optionResult === "USD"):
+                exchangeResult = input * 0.25;
+                reslut.innerText = exchangeResult;
+                break;
 
-        case (optionInput === "PLN" && optionResult === "USD"):
-            exchangeResult = input * 0.25;
-            reslut.innerText = exchangeResult;
-            break;
-
-        case (optionInput === "PLN" && optionResult === "EUR"):
-            exchangeResult = input * 0.23;
-            reslut.innerText = exchangeResult;
-            break;
-        default:
-            console.log("brak wybranej waluty");
+            case (optionInput === "PLN" && optionResult === "EUR"):
+                exchangeResult = input * 0.23;
+                reslut.innerText = exchangeResult;
+                break;
+            default:
+                console.log("brak wybranej waluty");
+        }
     }
-
+    change();
+    welcome();
 })
