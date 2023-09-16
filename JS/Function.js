683b77ab1466bccb0febbcc93448aad9e4efc48a
Jakub Kulik <jakub.kulik1@o2.pl>
2023-09-16 19:58:40 +0200

    more clear background, made order in JS

--- JS/Function.js
+++ JS/Function.js
@@ -1,73 +1,79 @@
-let formElement = document.querySelector(".js-form");
-let inputElement = document.querySelector(".js-input");
-let optionInputElement = document.querySelector(".js-optionInput");
-let optionResultElement = document.querySelector(".js-option-result");
-let reslut = document.querySelector(".js-result");
-
-
-const welcome = () => {
-    console.log("Hello Developer");
-}
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let input = inputElement.value;
-    let optionInput = optionInputElement.value;
-    let optionResult = optionResultElement.value;
-    let exchangeResult = 0;
-
-    const change = () => {
-        switch (true) {
-            case (optionInput === "EUR" && optionResult === "EUR"):
-                exchangeResult = input * 1;
-                reslut.innerText = exchangeResult;
-                break;
-
-            case (optionInput === "EUR" && optionResult === "USD"):
-                exchangeResult = input * 1.1;
-                reslut.innerText = exchangeResult;
-                break;
-
-            case (optionInput === "EUR" && optionResult === "PLN"):
-                exchangeResult = input * 4.43;
-                reslut.innerText = exchangeResult;
-                break;
-
-            case (optionInput === "USD" && optionResult === "USD"):
-                exchangeResult = input * 1;
-                reslut.innerText = exchangeResult;
-                break;
-
-            case (optionInput === "USD" && optionResult === "EUR"):
-                exchangeResult = input * 0.91;
-                reslut.innerText = exchangeResult;
-
-                break;
-
-            case (optionInput === "USD" && optionResult === "PLN"):
-                exchangeResult = input * 4.04;
-                reslut.innerText = exchangeResult;
-                break;
-
-            case (optionInput === "PLN" && optionResult === "PLN"):
-                exchangeResult = input * 1;
-                reslut.innerText = exchangeResult;
-
-                break;
-
-            case (optionInput === "PLN" && optionResult === "USD"):
-                exchangeResult = input * 0.25;
-                reslut.innerText = exchangeResult;
-                break;
-
-            case (optionInput === "PLN" && optionResult === "EUR"):
-                exchangeResult = input * 0.23;
-                reslut.innerText = exchangeResult;
-                break;
-            default:
-                console.log("brak wybranej waluty");
-        }
+{
+    const formElement = document.querySelector(".js-form");
+    const inputElement = document.querySelector(".js-input");
+    const optionInputElement = document.querySelector(".js-optionInput");
+    const optionResultElement = document.querySelector(".js-optionResult");
+    const result = document.querySelector(".js-result");
+   
+    const welcome = () => {
+        console.log("Hello Developer");
     }
-    change();
-    welcome();
-})
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+       
+        let input = inputElement.value;                 // value can change that`s why there is let instead of const
+        let optionInput = optionInputElement.value;     // value can change that`s why there is let instead of const
+        let optionResult = optionResultElement.value;   // value can change that`s why there is let instead of const
+
+
+        const calculateResult = () => {
+            switch (true) {
+                case (optionInput === "EUR" && optionResult === "EUR"):
+                    exchangeResult = input * 1;
+                    result.innerText = exchangeResult;
+                    break;
+
+                case (optionInput === "EUR" && optionResult === "USD"):
+                    exchangeResult = input * 1.1;
+                    result.innerText = exchangeResult;
+                    break;
+
+                case (optionInput === "EUR" && optionResult === "PLN"):
+                    exchangeResult = input * 4.43;
+                    result.innerText = exchangeResult;
+                    break;
+
+                case (optionInput === "USD" && optionResult === "USD"):
+                    exchangeResult = input * 1;
+                    result.innerText = exchangeResult;
+                    break;
+
+                case (optionInput === "USD" && optionResult === "EUR"):
+                    exchangeResult = input * 0.91;
+                    result.innerText = exchangeResult;
+                    break;
+
+                case (optionInput === "USD" && optionResult === "PLN"):
+                    exchangeResult = input * 4.04;
+                    result.innerText = exchangeResult;
+                    break;
+
+                case (optionInput === "PLN" && optionResult === "PLN"):
+                    exchangeResult = input * 1;
+                    result.innerText = exchangeResult;
+                    break;
+
+                case (optionInput === "PLN" && optionResult === "USD"):
+                    exchangeResult = input * 0.25;
+                    result.innerText = exchangeResult;
+                    break;
+
+                case (optionInput === "PLN" && optionResult === "EUR"):
+                    exchangeResult = input * 0.23;
+                    result.innerText = exchangeResult;
+                    break;
+                default:
+                    console.log("brak wybranej waluty");
+            }
+        }
+        calculateResult();
+    }
+
+    const init = () => {
+        welcome();
+        formElement.addEventListener("submit", onFormSubmit);
+    }
+
+    init();
+}
